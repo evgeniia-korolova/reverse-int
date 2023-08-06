cd381e79b5a9f82a998ce9b9bec3501866c9fee6
evgeniia-korolova <jkod@ukr.net>
2023-08-06 15:30:38 +0300

    feat : add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+     let number = Math.abs(n.toString());
+     let string = number.toString();
+     return Number(string.split("").reverse().join(""));
+    
 }
